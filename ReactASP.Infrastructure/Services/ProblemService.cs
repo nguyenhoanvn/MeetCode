9602930ec52f68e3955dd57e3ba0c7c2bdfac7b3
nguyenhoanvn <hoanyu12345@gmail.com>
2025-10-08 23:09:00 +0800

    add: ProblemService and UserService implemenations in Infrastructure layer

---
+++ ReactASP.Infrastructure/Services/ProblemService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Core;
+using ReactASP.Application.Interfaces;
+using ReactASP.Application.Interfaces.Services;
+using ReactASP.Domain.Entities;
+
+namespace ReactASP.Infrastructure.Services
+{
+    public class ProblemService : IProblemService
+    {
+        private readonly IProblemRepository _problemRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        public ProblemService(
+            IProblemRepository problemRepository,
+            IUnitOfWork unitOfWork)
+        {
+            _problemRepository = problemRepository;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<Problem> CreateProblemAsync(string title, string statementMd, string difficulty, int timeLimitMs, int memoryLimitMb, Guid userId, CancellationToken ct)
+        {
+            // Create new problem
+            var newProblem = new Problem
+            {
+                ProblemId = Guid.NewGuid(),
+                Title = title,
+                StatementMd = statementMd,
+                Difficulty = difficulty,
+                TimeLimitMs = timeLimitMs,
+                MemoryLimitMb = memoryLimitMb,
+                CreatedBy = userId,
+                CreatedAt = DateTimeOffset.UtcNow
+            };
+
+            await _problemRepository.AddAsync(newProblem, ct);
+
+            // Check saved
+            var saved = await _unitOfWork.SaveChangesAsync(ct);
+            if (saved <= 0)
+            {
+                throw new InvalidOperationException("Failed to add new refresh token");
+            }
+            return newProblem;
+        }
+    }
+}
---
+++ ReactASP.Infrastructure/Services/UserService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ardalis.Result;
+using Azure.Core;
+using ReactASP.Application.Interfaces;
+using ReactASP.Application.Interfaces.Services;
+using ReactASP.Domain.Entities;
+
+namespace ReactASP.Infrastructure.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public async Task<User> FindUserByEmailAsync(string email, CancellationToken ct)
+        {
+            var user = await _userRepository.GetUserByEmailWithTokensAsync(email, ct);
+
+            if (user == null)
+            {
+                throw new InvalidOperationException("User not found");
+            }
+            return user;
+        }
+
+        public bool IsPasswordMatch(string requestPassword, string userDbPassword)
+        {
+            if (requestPassword == null || userDbPassword == null)
+            {
+                throw new InvalidOperationException("Password input is null");
+            }
+            
+            return BCrypt.Net.BCrypt.Verify(requestPassword, userDbPassword);  
+        }
+
+        public async Task<User> FindUserAsync(Guid userId, CancellationToken ct)
+        {
+            var user = await _userRepository.GetByIdAsync(userId, ct);
+            if (user == null)
+            {
+                throw new InvalidOperationException($"User cannot be found with id: {userId}");
+            }
+            return user;
+        }
+    }
+}
