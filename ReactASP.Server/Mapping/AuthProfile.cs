09e0979bc1f3ce1e640142404f999ec836595838
nguyenhoanvn <hoanyu12345@gmail.com>
2025-10-09 23:09:02 +0800

    feat(mapping): add mapping configuration directory

---
+++ ReactASP.Server/Mapping/AuthProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using ReactASP.Application.Commands.LoginUser;
+using ReactASP.Application.Commands.RefreshToken;
+using ReactASP.Application.Commands.RegisterUser;
+using ReactASP.Application.DTOs.LoginUser;
+using ReactASP.Application.DTOs.RefreshToken;
+using ReactASP.Application.DTOs.RegisterUser;
+using ReactASP.Server.DTOs.RefreshToken;
+
+namespace ReactASP.Server.Mapping
+{
+    public class AuthProfile : Profile
+    {
+        public AuthProfile()
+        {
+            // Login
+            CreateMap<LoginRequest, LoginUserCommand>();
+            CreateMap<LoginUserResult, LoginResponse>();
+
+            // Register
+            CreateMap<RegisterRequest, RegisterUserCommand>();
+            CreateMap<RegisterUserResult, RegisterResponse>();
+
+            // RefreshToken
+            CreateMap<RefreshTokenRequest, RefreshTokenCommand>();
+            CreateMap<RefreshTokenResult, RefreshTokenResponse>();
+        }
+    }
+}
---
+++ ReactASP.Server/Mapping/ProblemProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ReactASP.Application.Commands.ProblemAdd;
+using ReactASP.Server.DTOs.ProblemAdd;
+
+namespace ReactASP.Server.Mapping
+{
+    public class ProblemProfile : Profile
+    {
+        public ProblemProfile()
+        {
+            // Problem add
+            CreateMap<ProblemAddRequest, ProblemAddCommand>();
+            CreateMap<ProblemAddResult, ProblemAddResponse>();
+        }
+    }
+}
