83ef6c9a177068dfbd0b895fc6c6532eb1835c4d
nguyenhoanvn <hoanyu12345@gmail.com>
2025-10-11 00:50:25 +0800

    test: add test cases for command validators

---
+++ ReactASP.Tests/Validator/LoginCommandValidatorTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation.TestHelper;
+using ReactASP.Application.Commands.LoginUser;
+using ReactASP.Application.Commands.RegisterUser;
+
+namespace ReactASP.Tests.Validator
+{
+    public class LoginCommandValidatorTests
+    {
+        private readonly string EMAIL_DUMMY = "hoanyu12345@gmail.com";
+        private readonly string PASSWORD_DUMMY = "hoanyu123";
+        private readonly LoginUserCommandValidator _validator;
+        public LoginCommandValidatorTests()
+        {
+            _validator = new LoginUserCommandValidator();
+        }
+
+        [Fact]
+        public async void ShouldHaveError_WhenEmailIsEmpty()
+        {
+            var model = new LoginUserCommand(
+                "",
+                PASSWORD_DUMMY
+            );
+            var result = await _validator.TestValidateAsync(model);
+            result.ShouldHaveValidationErrorFor(x => x.Email)
+                .WithErrorMessage("Email is required");
+        }
+
+        [Fact]
+        public async void ShouldHaveError_WhenEmailNotInFormat()
+        {
+            var model = new LoginUserCommand(
+                "hoanyu123",
+                PASSWORD_DUMMY
+            );
+            var result = await _validator.TestValidateAsync(model);
+            result.ShouldHaveValidationErrorFor(x => x.Email)
+                .WithErrorMessage("Email must have less than 255 characters");
+        }
+
+        [Fact]
+        public async void ShouldHaveError_WhenEmailOverMaximumLength()
+        {
+            var model = new LoginUserCommand(
+                new string('m', 1000) + "@gmail.com",
+                PASSWORD_DUMMY
+            );
+            var result = await _validator.TestValidateAsync(model);
+            result.ShouldHaveValidationErrorFor(x => x.Email)
+                .WithErrorMessage("Email must have less than 255 characters");
+        }
+
+        [Fact]
+        public async void ShouldHaveError_WhenPasswordIsEmpty()
+        {
+            var model = new LoginUserCommand(
+                EMAIL_DUMMY,
+                ""
+            );
+            var result = await _validator.TestValidateAsync(model);
+            result.ShouldHaveValidationErrorFor(x => x.Password)
+                .WithErrorMessage("Password is required");
+        }
+
+        [Fact]
+        public async void ShouldHaveError_WhenPasswordLessThanMinimumLength()
+        {
+            var model = new LoginUserCommand(
+                EMAIL_DUMMY,
+                "abc123"
+            );
+            var result = await _validator.TestValidateAsync(model);
+            result.ShouldHaveValidationErrorFor(x => x.Password)
+                .WithErrorMessage("Password must have more than 8 characters.");
+        }
+
+        [Fact]
+        public async void ShouldNotHaveError_WhenAllFieldsAreValid()
+        {
+            var model = new LoginUserCommand(
+                EMAIL_DUMMY,
+                PASSWORD_DUMMY
+            );
+            var result = await _validator.TestValidateAsync(model);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
--- ReactASP.Tests/Validator/RegisterCommandValidatorTests.cs
+++ ReactASP.Tests/Validator/RegisterCommandValidatorTests.cs
@@ -60,5 +60,95 @@ namespace ReactASP.Tests.Validator
             result.ShouldHaveValidationErrorFor(x => x.Email)
                 .WithErrorMessage("Email must not greater than 255 characters.");
         }
+
+        [Fact]
+        public async void ShouldHaveError_WhenDisplayNameEmpty()
+        {
+            var model = new RegisterUserCommand(
+                EMAIL_DUMMY,
+                "",
+                PASSWORD_DUMMY);
+
+            var result = await _validator.TestValidateAsync(model);
+            result.ShouldHaveValidationErrorFor(x => x.DisplayName)
+                .WithErrorMessage("Display name is required.");
+        }
+
+        [Fact]
+        public async void ShouldHaveError_WhenDisplayNameOverMaximumLength()
+        {
+            var model = new RegisterUserCommand(
+                EMAIL_DUMMY,
+                new string('n', 101),
+                PASSWORD_DUMMY
+            );
+            var result = await _validator.TestValidateAsync(model);
+            result.ShouldHaveValidationErrorFor(x => x.DisplayName)
+                .WithErrorMessage("Display name must not greater than 100 characters.");
+        }
+
+        [Fact]
+        public async void ShouldHaveError_WhenPasswordIsEmpty()
+        {
+            var model = new RegisterUserCommand(
+                EMAIL_DUMMY,
+                DISPLAY_NAME_DUMMY,
+                ""
+            );
+            var result = await _validator.TestValidateAsync(model);
+            result.ShouldHaveValidationErrorFor(x => x.Password)
+                .WithErrorMessage("Password is required.");
+        }
+
+        [Fact]
+        public async void ShouldHaveError_WhenPasswordLessThanMinimumLength()
+        {
+            var model = new RegisterUserCommand(
+                EMAIL_DUMMY,
+                DISPLAY_NAME_DUMMY,
+                "abc123"
+            );
+            var result = await _validator.TestValidateAsync(model);
+            result.ShouldHaveValidationErrorFor(x => x.Password)
+                .WithErrorMessage("Password must have more than 8 characters.");
+        }
+
+        [Fact]
+        public async void ShouldHaveError_WhenPasswordLacksLowercase()
+        {
+            var model = new RegisterUserCommand(
+                EMAIL_DUMMY,
+                DISPLAY_NAME_DUMMY,
+                "HOANYU12345"
+            );
+            var result = await _validator.TestValidateAsync(model);
+            result.ShouldHaveValidationErrorFor(x => x.Password)
+                .WithErrorMessage("Password must contain at least one lowercase letter.");
+        }
+
+        [Fact]
+        public async void ShouldHaveError_WhenPasswordLacksNumber()
+        {
+            var model = new RegisterUserCommand(
+                EMAIL_DUMMY,
+                DISPLAY_NAME_DUMMY,
+                "hoanyuabcde"
+            );
+            var result = await _validator.TestValidateAsync(model);
+            result.ShouldHaveValidationErrorFor(x => x.Password)
+                .WithErrorMessage("Password must contain at least one number.");
+        }
+
+        [Fact]
+        public async void ShouldNotHaveError_WhenAllFieldsAreValid()
+        {
+            var model = new RegisterUserCommand(
+                EMAIL_DUMMY,
+                DISPLAY_NAME_DUMMY,
+                "hoanyu123"
+            );
+            var result = await _validator.TestValidateAsync(model);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
     }
 }
