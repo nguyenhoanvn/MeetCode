aebd1e6a4281ab73b741c36fab69cf4b39a4bfae
nguyenhoanvn <hoanyu12345@gmail.com>
2025-10-15 23:45:17 +0800

    chore: create folder in middleware in Server layer

---
+++ ReactASP.Server/Middlewares/ExceptionHandler.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Data.SqlClient;
+using StackExchange.Redis;
+
+namespace ReactASP.Server.Middlewares
+{
+    public class ExceptionHandler
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandler> _logger;
+
+        public ExceptionHandler(
+            RequestDelegate next,
+            ILogger<ExceptionHandler> logger
+            )
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unhandled exception at {Path}", context.Request.Path);
+
+                var status = ex switch
+                {
+                    InvalidOperationException => StatusCodes.Status400BadRequest,
+                    BadHttpRequestException => StatusCodes.Status400BadRequest,
+                    UnauthorizedAccessException => StatusCodes.Status401Unauthorized,
+                    SqlException => StatusCodes.Status500InternalServerError,
+                    TimeoutException => StatusCodes.Status504GatewayTimeout,
+                    RedisConnectionException => StatusCodes.Status503ServiceUnavailable,
+                    HttpRequestException => StatusCodes.Status502BadGateway,
+                    _ => StatusCodes.Status500InternalServerError
+                };
+
+                var response = new
+                {
+                    error = ex.Message,
+                    status
+                };
+
+                context.Response.StatusCode = status;
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsJsonAsync(response);
+            }
+        }
+    }
+}
