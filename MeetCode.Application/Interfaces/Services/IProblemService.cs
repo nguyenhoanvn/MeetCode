dc093e2f35f16ec9bb6358ff18c3df4a8ce46b3a
nguyenhoanvn <hoanyu12345@gmail.com>
2025-10-23 00:02:54 +0800

    refactor: update IProblemService interface

--- MeetCode.Application/Interfaces/Services/IProblemService.cs
+++ MeetCode.Application/Interfaces/Services/IProblemService.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MeetCode.Application.Commands.CommandEntities.Problem;
 using MeetCode.Domain.Entities;
 
 namespace MeetCode.Application.Interfaces.Services
 {
     public interface IProblemService
     {
-        Task<Problem> CreateProblemAsync(string title, string statementMd, string difficulty, int timeLimitMs, int memoryLimitMb, Guid createdBy, CancellationToken ct);
+        Task<Problem> CreateProblemAsync(string title, string statementMd, string difficulty, int timeLimitMs, int memoryLimitMb, Guid createdBy, List<Guid> tagIds, CancellationToken ct);
         Task<IEnumerable<Problem>> ReadAllProblemsAsync(CancellationToken ct);
         Task<Problem?> FindProblemByIdAsync(Guid problemId, CancellationToken ct);
         Task<Problem?> FindProblemBySlugAsync(string problemSlug, CancellationToken ct);
-        Task<Problem?> UpdateProblemAsync(Problem newProblem, CancellationToken ct);
+        Task<Problem?> UpdateProblemAsync(ProblemUpdateCommand problem, CancellationToken ct);
         Task DeleteProblemAsync(Problem problemToDelete, CancellationToken ct);
         
     }
