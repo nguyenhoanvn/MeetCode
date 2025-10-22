b364187b886f376812204452ce7b8490d8d0f038
nguyenhoanvn <hoanyu12345@gmail.com>
2025-10-22 23:58:58 +0800

    feat(domain): add custom exceptions for business rule violations

---
+++ MeetCode.Domain/Exceptions/DuplicateEntityException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetCode.Domain.Exceptions
+{
+    public class DuplicateEntityException<TEntity> : Exception
+    {
+        public string FieldName { get; }
+        public string FieldValue { get; }
+        public DuplicateEntityException(string fieldName, string fieldValue)
+            : base($"{typeof(TEntity).Name} with {fieldName} '{fieldValue}' already exists") 
+        {
+            FieldName = fieldName;
+            FieldValue = fieldValue;
+        }
+    }
+}
---
+++ MeetCode.Domain/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetCode.Domain.Exceptions
+{
+    public class EntityNotFoundException<TEntity> : Exception
+    {
+        public string FieldName { get; }
+        public string FieldValue { get; }
+
+        public EntityNotFoundException(string fieldName, string fieldValue)
+            : base($"{typeof(TEntity).Name} with '{fieldName}' {fieldValue} cannot found")
+        {
+            FieldName = fieldName;
+            FieldValue = fieldValue;
+        }
+    }
+}
