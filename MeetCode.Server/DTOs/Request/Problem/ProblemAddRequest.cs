67fff3922120bf8206e4aaec498206bfa49f4b5c
nguyenhoanvn <hoanyu12345@gmail.com>
2025-10-23 23:09:28 +0800

    refactor: modify the request of some problem endpoints

--- MeetCode.Server/DTOs/Request/Problem/ProblemAddRequest.cs
+++ MeetCode.Server/DTOs/Request/Problem/ProblemAddRequest.cs
@@ -1,11 +1,14 @@
-﻿namespace MeetCode.Server.DTOs.Request.Problem
+﻿using MeetCode.Domain.Entities;
+
+namespace MeetCode.Server.DTOs.Request.Problem
 {
     public sealed record ProblemAddRequest(
         string Title, 
         string StatementMd,
         string Difficulty,
         int TimeLimitMs,
-        int MemoryLimitMb
+        int MemoryLimitMb,
+        List<Guid> TagIds
         );
 
 }
---
+++ MeetCode.Server/DTOs/Request/Problem/ProblemUpdateRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace MeetCode.Server.DTOs.Request.Problem
+{
+    public sealed record ProblemUpdateRequest(
+        string NewTitle,
+        string NewStatementMd,
+        string NewDifficulty,
+        IEnumerable<Guid> TagIds
+        );
+}
