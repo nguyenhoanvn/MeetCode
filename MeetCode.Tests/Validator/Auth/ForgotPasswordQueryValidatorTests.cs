043d7c9b58c1d78cdb33c9ea29aa00baa52a8f06
nguyenhoanvn <hoanyu12345@gmail.com>
2025-10-25 23:38:12 +0800

    test: add validators test cases

---
+++ MeetCode.Tests/Validator/Auth/ForgotPasswordQueryValidatorTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation.TestHelper;
+using MeetCode.Application.Queries.QueryEntities.Auth;
+using MeetCode.Application.Queries.QueryValidators.Auth;
+
+namespace MeetCode.Tests.Validator.Auth
+{
+    public class ForgotPasswordQueryValidatorTests
+    {
+        private readonly string EMAIL_DUMMY = "hoanyu12345@gmail.com";
+        private readonly ForgotPasswordQueryValidator _validator;
+        public ForgotPasswordQueryValidatorTests()
+        {
+            _validator = new ForgotPasswordQueryValidator();
+        }
+
+        [Fact]
+        public async void ShouldHaveError_WhenEmailIsEmpty()
+        {
+            var model = new ForgotPasswordQuery("");
+            var result = await _validator.TestValidateAsync(model);
+            result.ShouldHaveValidationErrorFor(x => x.Email);
+        }
+
+        [Fact]
+        public async void ShouldHaveError_WhenEmailNotInFormat()
+        {
+            var model = new ForgotPasswordQuery("hoanyu12345");
+            var result = await _validator.TestValidateAsync(model);
+            result.ShouldHaveValidationErrorFor(x => x.Email);
+        }
+
+        [Fact]
+        public async void ShouldNotHaveError_WhenEmailIsValid()
+        {
+            var model = new ForgotPasswordQuery(EMAIL_DUMMY);
+            var result = await _validator.TestValidateAsync(model);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
