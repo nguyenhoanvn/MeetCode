e17bae855718df7f0d3068e08afed42bf93a3e48
nguyenhoanvn <hoanyu12345@gmail.com>
2025-11-04 23:25:21 +0800

    chore: remove testing pages

--- meetcode.client/src/pages/LoginPage.jsx
+++
@@ -1,7 +0,0 @@
-import LoginForm from "../components/LoginForm";
-import {useAuth} from "../hooks/useAuth";
-
-export default function LoginPage() {
-    const {handleLogin} = useAuth();
-    return <LoginForm onLogin={handleLogin}/>;
-}
