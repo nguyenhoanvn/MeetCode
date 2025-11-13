b6208310b3fce59578febf3ae2cedaab3a414745
nguyenhoanvn <hoanyu12345@gmail.com>
2025-11-13 23:44:12 +0800

    feat: result logging for each endpoint for administrator use

---
+++ MeetCode.Server/Middlewares/HandlerResultLoggingBehavior.cs
@@ -0,0 +1,34 @@
+﻿using Ardalis.Result;
+using MediatR;
+
+namespace MeetCode.Server.Middlewares
+{
+    public class HandlerResultLoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger<HandlerResultLoggingBehavior<TRequest, TResponse>> _logger;
+
+        public HandlerResultLoggingBehavior(ILogger<HandlerResultLoggingBehavior<TRequest, TResponse>> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken ct)
+        {
+            var response = await next();
+
+            var responseType = response.GetType();
+
+            if (responseType.IsGenericType && responseType.GetGenericTypeDefinition() == typeof(Result<>))
+            {
+                object value = responseType.GetProperty("Value")?.GetValue(response);
+                _logger.LogInformation("Handler {Handler} returned with result {@Value}", typeof(TRequest).Name, value);
+            }
+            else
+            {
+                _logger.LogInformation("Handler {Handler} returned {@Response}", typeof(TRequest).Name, response);
+            }
+
+            return response;
+        }
+    }
+}
