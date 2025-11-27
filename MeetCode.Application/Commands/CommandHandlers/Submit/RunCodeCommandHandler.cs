e40d54598385fc3958be77f5ff5ffe1d88a75f02
nguyenhoanvn <hoanyu12345@gmail.com>
2025-11-27 22:41:01 +0700

    feat: run code message enqueuing endpoint

---
+++ MeetCode.Application/Commands/CommandHandlers/Submit/RunCodeCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using Ardalis.Result;
+using MediatR;
+using MeetCode.Application.Commands.CommandEntities.Submit;
+using MeetCode.Application.Commands.CommandResults.Submit;
+using MeetCode.Application.DTOs.Other;
+using MeetCode.Application.Interfaces.Services;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetCode.Application.Commands.CommandHandlers.Submit
+{
+    public class RunCodeCommandHandler : IRequestHandler<RunCodeCommand, Result<EnqueueResult>>
+    {
+        private readonly IJobSenderService _jobSender;
+        private readonly ILogger<RunCodeCommandHandler> _logger;
+        public RunCodeCommandHandler(
+            IJobSenderService jobSender,
+            ILogger<RunCodeCommandHandler> logger)
+        {
+            _jobSender = jobSender;
+            _logger = logger;
+        }
+        public async Task<Result<EnqueueResult>> Handle(RunCodeCommand request, CancellationToken ct)
+        {
+            try
+            {
+                await _jobSender.EnqueueJobAsync<RunCodeCommand>(request, "run_code_queue", ct);
+                var result = new EnqueueResult(request, "run_code_queue");
+                return Result.Success(result);
+            } catch (Exception ex)
+            {
+                return Result.Error("Failed to enqueue message:" + ex.Message);
+            }
+        }
+    }
+}
---
+++ MeetCode.Server/Controllers/SubmitController.cs
@@ -0,0 +1,33 @@
+﻿using Ardalis.Result.AspNetCore;
+using Microsoft.AspNetCore.Http;
+using Ardalis.Result;
+using Microsoft.AspNetCore.Mvc;
+using MeetCode.Application.DTOs.Request.Submit;
+using MediatR;
+using AutoMapper;
+using MeetCode.Application.Commands.CommandEntities.Submit;
+using MeetCode.Application.DTOs.Other;
+
+namespace MeetCode.Server.Controllers
+{
+    [Route("submit")]
+    [ApiController]
+    public class SubmitController : ControllerBase
+    {
+        private readonly ISender _mediator;
+        private readonly IMapper _mapper;
+
+        [HttpPost("run")]
+        [TranslateResultToActionResult]
+        [ProducesResponseType(typeof(EnqueueResult), StatusCodes.Status202Accepted)]
+        [ExpectedFailures(ResultStatus.Error)]
+        public async Task<Result<EnqueueResult>> RunCode(RunCodeRequest request, CancellationToken ct)
+        {
+            var cmd = _mapper.Map<RunCodeCommand>(request);
+
+            var result = await _mediator.Send(cmd, ct);
+
+            return result;
+        }
+    }
+}
