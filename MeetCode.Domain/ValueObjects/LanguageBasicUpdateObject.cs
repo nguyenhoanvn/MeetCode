1691d21a6b6d81e7696e1f77bdd48482b4de3568
nguyenhoanvn <hoanyu12345@gmail.com>
2025-12-20 04:40:48 +0700

    feat: added value object for parsing application request object to domain object for better handling

---
+++ MeetCode.Domain/ValueObjects/LanguageBasicUpdateObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetCode.Domain.ValueObjects
+{
+    public sealed record LanguageBasicUpdateObject(
+        string? Name,
+        string? Version,
+        string? FileExtension,
+        string? CompileImage,
+        string? RuntimeImage,
+        string? CompileCommand,
+        string? RunCommand
+    );
+}
